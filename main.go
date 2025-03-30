04df0e7e533a83c23998058a0a25c1dafdb85e3b
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2025-03-31 01:02:24 +0530

    Initialize Redis client in main application

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"url-shortener/utils"
 	"net/http"
 	"url-shortener/routes"
 )
@@ -11,7 +12,7 @@ import (
 func main(){
 	fmt.Println("Starting Url Shortener...")
 	fmt.Println("Server starting on port 5000")
-
+	utils.InitRedis()
 	routes.RegisterRoutes()
 	err := http.ListenAndServe(":5000", nil)
 	if err != nil {
