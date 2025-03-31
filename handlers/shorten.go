303ce3eb5118cc9e70d4ee1aeec469be8105d9ec
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2025-03-31 11:46:01 +0530

    Implement Redis storage for shortened URLs and enhance error handling in CreateUrl function

--- handlers/shorten.go
+++ handlers/shorten.go
@@ -1,24 +1,34 @@
 package handlers
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"log"
 	"time"
-	"url-shortener/models"
 	"url-shortener/utils"
-
 )
 
 func CreateUrl(url string) string {	 // create a new url(call the hash vaala fn) and store it in the database(local map)
 	shortUrl := utils.HashUrl(url)
-	id := shortUrl
-	models.UrlDb[id] = models.Url{
-		Id: id,
-		OriginalUrl: url,
-		ShortenedUrl: shortUrl,
-		CreationDate: time.Now(),
+	ctx := context.Background()
+	err := utils.RedisClient.Set(ctx, shortUrl, url, 24*time.Hour).Err()
+
+	if err != nil {
+		fmt.Println("Error storing URL in Redis:", err)
+		return ""
 	}
+
+
+	
+	// id := shortUrl
+	// models.UrlDb[id] = models.Url{
+	// 	Id: id,
+	// 	OriginalUrl: url,
+	// 	ShortenedUrl: shortUrl,
+	// 	CreationDate: time.Now(),
+	// }
 	return shortUrl
 }
 
@@ -33,6 +43,10 @@ func ShortUrlHandler(w http.ResponseWriter, r *http.Request){	// w is used to wr
 		return
 	}
 	shortUrl := CreateUrl(data.URL)
+	if shortUrl == ""{
+		http.Error(w, "Error storing in redis", http.StatusInternalServerError)
+	}
+
 	// response := map[string]string{"shortened_url": shortUrl}
 	// jsonResponse, err := json.Marshal(response)
 	// if err != nil {
@@ -51,6 +65,9 @@ func ShortUrlHandler(w http.ResponseWriter, r *http.Request){	// w is used to wr
 	if err != nil {
 		http.Error(w, "Error generating Json Response", http.StatusBadRequest)
 	}
-	fmt.Println("Shortner In-Memory Content: \n ", models.UrlDb)
+	ctx := context.Background()
+	dbs := utils.RedisClient.Keys(ctx, "*")
+	log.Printf("%s Url successfully shortened", data.URL)
+	fmt.Println("Shortner In-Memory Content: \n ", dbs)
 }
 
